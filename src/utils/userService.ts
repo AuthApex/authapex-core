1b6a508fc0164d2d8dce0af985feadf5257a1672
Radim Staněk <gtomyasek@gmail.com>
2025-10-03 18:50:38 +0200

    feat: integrate `date-fns` for cache expiration in `UserService` and enhance caching mechanisms

--- src/utils/userService.ts
+++ src/utils/userService.ts
@@ -1,16 +1,22 @@
 import { SimpleUser, User, UserVariable } from '@/models/user';
 import axios from 'axios';
+import { addHours, isAfter } from 'date-fns';
 
 export class UserService {
+  private cacheTime: Date = new Date();
+
   private readonly cache = new Map<string, User>();
+  private readonly simpleUserCache = new Map<string, SimpleUser>();
 
   constructor(private readonly authApi: string) {}
 
   public addUserToCache(sessionId: string, user: User): void {
+    this.checkAndInvalidateOldCache();
     this.cache.set(sessionId, user);
   }
 
   public getUserFromCache(sessionId: string): User | undefined {
+    this.checkAndInvalidateOldCache();
     return this.cache.get(sessionId);
   }
 
@@ -22,7 +28,23 @@ export class UserService {
     };
   }
 
-  public async getUserData(userId: string): Promise<SimpleUser> {
-    return axios.get<SimpleUser>(this.authApi + '/api/user?userId=' + userId).then((res) => res.data);
+  public async getSimpleUser(userId: string): Promise<SimpleUser> {
+    this.checkAndInvalidateOldCache();
+    if (this.simpleUserCache.has(userId)) {
+      return this.simpleUserCache.get(userId)!;
+    }
+    const simpleUser = await axios.get<SimpleUser>(this.authApi + '/api/user?userId=' + userId).then((res) => res.data);
+
+    this.simpleUserCache.set(userId, simpleUser);
+
+    return simpleUser;
+  }
+
+  private checkAndInvalidateOldCache(): void {
+    if (isAfter(new Date(), addHours(this.cacheTime, 1))) {
+      this.cache.clear();
+      this.simpleUserCache.clear();
+      this.cacheTime = new Date();
+    }
   }
 }
